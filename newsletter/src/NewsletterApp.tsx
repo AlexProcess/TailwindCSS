c35cbe94cad3c95419c489ce692ffc694da9ee2d
AlexProcess <alexsocialmediaworks@gmail.com>
2023-09-15 15:04:03 +0200

    style: :lipstick: mock FormSuccess

--- newsletter/src/NewsletterApp.tsx
+++ newsletter/src/NewsletterApp.tsx
@@ -4,35 +4,37 @@ import { BgMobile } from "./assets/images/illustration-sign-up-mobile";
 import { BgDesktop } from "./assets/images/illustration-sign-up-desktop";
 import { InitialText } from "./components/UI/InitialText";
 import { Form } from "./components/Form/Form";
+import { FormSuccess } from "./components/Form/FormSuccess";
 
 interface NewsletterProps {}
 
 export const NewsletterApp: React.FC<NewsletterProps> = ({}) => {
-    const [isDesktop, setIsDesktop] = useState(false);
+    // const [isDesktop, setIsDesktop] = useState(false);
 
-    useEffect(() => {
-        // Verificar la resolución de pantalla
-        const handleResize = () => {
-            setIsDesktop(window.innerWidth >= 1440);
-        };
+    // useEffect(() => {
+    //     // Verificar la resolución de pantalla
+    //     const handleResize = () => {
+    //         setIsDesktop(window.innerWidth >= 1440);
+    //     };
 
-        // Agregar un event listener para cambiar la resolución
-        window.addEventListener("resize", handleResize);
+    //     // Agregar un event listener para cambiar la resolución
+    //     window.addEventListener("resize", handleResize);
 
-        // Llamar a handleResize una vez para establecer la resolución inicial
-        handleResize();
+    //     // Llamar a handleResize una vez para establecer la resolución inicial
+    //     handleResize();
 
-        // Limpiar el event listener al desmontar el componente
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+    //     // Limpiar el event listener al desmontar el componente
+    //     return () => {
+    //         window.removeEventListener("resize", handleResize);
+    //     };
+    // }, []);
 
     return (
         <div>
-            {isDesktop ? <BgDesktop /> : <BgMobile />}
+            <FormSuccess />
+            {/* {isDesktop ? <BgDesktop /> : <BgMobile />}
             <InitialText />
-            <Form />
+            <Form /> */}
         </div>
     );
 };
--- newsletter/src/components/Form/FormSuccess.tsx
+++ newsletter/src/components/Form/FormSuccess.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { IconSuccess } from "../../assets/images/icon-success";
 
-export const FormSuccess = () => {
-    return <div>FormSuccess</div>;
+export const FormSuccess: React.FC = ({}) => {
+    return (
+        <div className="flex px-8 py-24">
+            <div className="flex-col items-center space-y-6">
+                <div className="mb-8">
+                    <IconSuccess />
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold text-charcoalgrey">
+                        Thanks for Subscribing!
+                    </h1>
+                </div>
+                <div>
+                    <p className="font-medium text-charcoalgrey">
+                        A confirmation email has been sent to {"email"}
+                        please open it and click the button inside to confirm *
+                        our subscription
+                    </p>
+                </div>
+                <button className="absolute px-12 py-3 font-bold text-white border border-blue-700 rounded-md left-5 right-5 bottom-8 bg-dark-slate hover:bg-blue-700">
+                    Dismiss messsage
+                </button>
+            </div>
+        </div>
+    );
 };
